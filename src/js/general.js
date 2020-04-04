d810186de09f260bf2127ef335bb1d0e9518a7c3
prikhodkin <prikhodkin_ilya@mail.ru>
2020-04-05 02:42:27 +0500

    Закончил футер, поправил мелкие недочеты

--- src/js/general.js
+++ src/js/general.js
@@ -5,3 +5,11 @@
 //= "../blocks/write/write.js"
 //= "../blocks/instagram/instagram.js"
 //= "../blocks/reviews/reviews.js"
+
+$(function(){
+  $("a[href^='#']").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+});
